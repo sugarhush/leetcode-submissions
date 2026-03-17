c1787508013ec3a706b6f5bfbf8fc47428dec0e4
sugarhush <hemansakkthivelm@gmail.com>
2026-03-17 12:16:39 +0000

    [LeetCode Sync] Runtime - 2 ms (20.05%), Memory - 21.7 MB (92.75%)

---
+++ my-folder/0268-missing-number/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        return ( (nums.size() * (nums.size() + 1) / 2) - std::accumulate(nums.begin(), nums.end(), 0) );
+    }
+};
